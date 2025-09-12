5c119fefac867221768709f11cc945672c719334
peterffyd <peterffyd@kkszki.hu>
2025-09-12 09:49:51 +0200

    Feladat9 is ready.

--- OopAlapok/Program.cs
+++ OopAlapok/Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OopAlapok{
     public class Szemely{
         protected string nev;
         private int kor;
 
-        public Szemely(string Nev){
-            this.nev = Nev;
-        }
         public int Kor{
             get => kor;
             set{
                 if (value <= 0)   throw new ArgumentException("Kor nem lehet negatív.");
                 else kor = value;
             }
+        }
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value;}
         }
         public override string ToString(){
             return $"A tanuló neve: {nev}, életkora: {kor}";
@@ -47,13 +50,25 @@ namespace OopAlapok{
     }
     internal class Program{
         static void Main(string[] args){
-            try{
-                Szemely tanulo = new Szemely("Kiss Péter");
-                tanulo.Kor = 32;
-                Console.Write(tanulo);
+            Szemely tanulo = new Szemely();
+            tanulo.Kor = 32;
+            tanulo.Nev = "Kiss Péter";
+            Console.WriteLine(tanulo);
+
+            List<Hallgato> nevsor = new List<Hallgato>();
+            for (int i = 0; i < 3; i++)
+            {
+                Hallgato h1 = new Hallgato();
+                Console.Write($"Kérem {i + 1}. nevet:");
+                h1.Nev = Console.ReadLine();
+                Console.Write($"Kérem {i + 1}. életkort:");
+                h1.Kor = int.Parse(Console.ReadLine());
+                nevsor.Add(h1);
+
             }
-            catch (ArgumentException ex){
-                Console.WriteLine($"Hiba: {ex.Message}");
+            foreach (var item in nevsor)
+            {
+                Console.WriteLine(item.Nev);
             }
         }
     }
