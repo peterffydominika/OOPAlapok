437558ff867f2202eb6797624fe4152767036b61
peterffyd <peterffyd@kkszki.hu>
2025-09-12 08:21:12 +0200

    Feladat2 modified

--- OopAlapok/Program.cs
+++ OopAlapok/Program.cs
@@ -1,42 +1,36 @@
 ﻿using System;
 
-namespace OopAlapok
-{
-    public class Szemely
-    {
-        public string nev;
+namespace OopAlapok{
+    public class Szemely{
+        private string nev;
         private int kor;
 
-        public Szemely(string nev, int kor)
-        {
-            this.nev = nev;
-            this.Kor = kor; // Use the Kor property to enforce validation
+        public Szemely(string Nev, int Kor){
+            this.nev = Nev;
+            this.kor = Kor;
         }
 
-        public int Kor
-        {
-            get => kor;
-            set
-            {
-                if (value <= 0)
-                    throw new ArgumentException("Kor nem lehet negatív.");
-                kor = value;
-            }
+        public string Kiir(){
+            return $"A tanulo neve: {nev}, életkora: {kor}";
         }
+        //public int Kor{
+        //    get => kor;
+        //    set{
+        //        if (value <= 0)
+        //            throw new ArgumentException("Kor nem lehet negatív.");
+        //        kor = value;
+        //    }
+        //}
     }
-
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
-                Szemely tanulo = new Szemely("Kiss Anna", 27);
-                Console.WriteLine(tanulo.nev);
-                Console.WriteLine(tanulo.Kor);
+    internal class Program{
+        static void Main(string[] args){
+            try{
+                Szemely tanulo = new Szemely("Kiss Péter", 35);
+                Console.WriteLine(tanulo.Kiir());
+                //Console.WriteLine(tanulo.nev);
+                //Console.WriteLine(tanulo.Kor);
             }
-            catch (ArgumentException ex)
-            {
+            catch (ArgumentException ex){
                 Console.WriteLine($"Hiba: {ex.Message}");
             }
         }
